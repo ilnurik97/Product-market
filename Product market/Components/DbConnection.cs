fd3971957495e821890fa21e36864df65fbd754f
Acer <Acer@DESKTOP-JFJ7FPE>
2022-12-02 18:12:04 +0300

    Добавьте файлы проекта.

---
+++ Product market/Components/DbConnection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Product_market.Components
+{
+    internal class DbConnection
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-JFJ7FPE;Initial Catalog=test;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return con;
+        }
+    }
+}
